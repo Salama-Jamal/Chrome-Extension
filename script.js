1957c5a9137bd193a829cb4bb0deaee0f1478fef
Jamal <mybusineess@gmail.com>
2026-05-03 04:03:22 +0100

    test template string ('literal')

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
-function saveText() {
-    console.log("Saving text...");
-}
+// function saveText() {
+//     console.log("Saving text...");
+// }
+
+
+
+
+
+// template strings/literals
+
+const recipient = "James"
+// Create a new variable, sender, and set its value to your name
+let sender = 'salama'
+// Use your sender variable instead of "Per"
+const email = `Hey ${recipient}! How is it going? Cheers ${sender}`
+
+console.log(email)
