bb09d29ab33e7b05cc4c7b782fe3977f7f271380
Jamal <mybusineess@gmail.com>
2026-05-14 19:52:00 +0100

    test something

--- script.js
+++ script.js
@@ -17,6 +17,7 @@ if (noteFromStorage) {
 
 
 
+
 saveTabBtn.addEventListener("click", function () {
     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
         myNote.push(tabs[0].url)
